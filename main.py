bda98e8bf28ed3924f6f02bdff718b0bfef8477a
Jose David <lujan9495@gmail.com>
2025-11-06 09:52:36 -0500

    Insert points into the collection

--- main.py
+++ main.py
@@ -11,14 +11,43 @@ print(f"Connected to Qdrant Cloud: {len(collections.collections)} collections" )
 # define the collection name 
 collection_name = "my_first_collection"
 
-# create the collection with specified vector parameters
-client.create_collection(
-  collection_name=collection_name,
-  vectors_config=models.VectorParams(
-    size = 4, # Dimensionality of the vectors
-    distance = models.Distance.COSINE # Distance metric to use for similarity search
-  )
-)
+# # create the collection with specified vector parameters
+# client.create_collection(
+#   collection_name=collection_name,
+#   vectors_config=models.VectorParams(
+#     size = 4, # Dimensionality of the vectors
+#     distance = models.Distance.COSINE # Distance metric to use for similarity search
+#   )
+# )
 
 collections = client.get_collections()
 print(f"Collection '{collection_name}' created. Total collections: {len(collections.collections)}")
+
+
+# Insert points into the collection
+points = [
+  models.PointStruct(
+    id = 1, 
+    vector = [0.1, 0.2, 0.3, 0.4],
+    payload = {
+        "category": "Demo"
+      }
+  ),
+  models.PointStruct(
+    id = 2, 
+    vector = [0.2, 0.3, 0.4, 0.5],
+    payload = {
+        "category": "example"
+      }
+  )
+]
+
+# insert vectors into the collection
+client.upsert(
+  collection_name=collection_name,
+  points=points
+)
+
+# retrieve collection details
+collection_info = client.get_collection(collection_name)
+print(f"Collection info:", collection_info)
